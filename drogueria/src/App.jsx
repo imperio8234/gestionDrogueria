9b31f865aeb3ea2a86d5c8bbdcc06b6cc8b7daf7
jhon anderson <garciacalvojhonanderson@gmail.com>
2023-05-09 09:37:53 -0500

    inicio de proyecto

---
+++ drogueria/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Main } from './componentes/registroInicio/principal'
+import "bootstrap";
+function App() {
+  
+  return (
+  <div className='app'>
+    <Main></Main>
+    
+  </div>
+  )
+}
+
+export default App
---
+++ drogueria/src/componentes/registroInicio/principal.jsx
@@ -0,0 +1,17 @@
+import "../../css/registroInicio/prin.css"
+
+export const Main=()=>{
+    return(
+        <div className="mainRegisterInicio">
+            <div className="containerForm">
+                
+            </div>
+            es
+            <div className="containerImg">
+                im
+                <img src="../../../public/drogueria.JPEG" alt="drogueria"/>
+            </div>
+
+        </div>
+    );
+};
